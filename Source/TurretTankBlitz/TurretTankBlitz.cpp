32eeff343ea0248462bc88998d8a7ffec9020f3f
Franz <FranzLor64@yahoo.com>
2024-06-06 15:28:50 -0600

    Added: BasePawn C++
    BasePawn class for player and NPCs

---
+++ Source/TurretTankBlitz/TurretTankBlitz.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TurretTankBlitz.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TurretTankBlitz, "TurretTankBlitz" );
